e36028a23f87a2ffe495d1ef91d0e609ad969461
Neil Shweky <neilshweky@gmail.com>
2020-09-23 17:57:45 -0400

    add cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const routes = require('./routes/routes.js');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
