ed374b5f8cf2db0bec32c6a1ecf75306a31f858b
Edward Kim <edkim41222@gmail.com>
2020-09-23 15:13:11 -0400

    removed db-config credentials

--- server/db-config.js
+++ server/db-config.js
@@ -1,7 +1,8 @@
 // Private. Will not be included in submission
 module.exports = {
-  host: 'fling.seas.upenn.edu',
-  user: 'kime022',
-  password: 'temppassword@',
-  database: 'kime022',
+  host: '',
+  user: '',
+  password: '',
+  database: '',
+  host: '',
 };
