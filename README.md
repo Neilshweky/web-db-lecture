dd035d83ce68d193d03fb4024cb332bc20946e6c
Neil Shweky <neilshweky@gmail.com>
2020-09-23 13:49:39 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# CIS550 Web DB Lecture
+
+Hi guys! This is the code for the lecture on Web DB stuff.
+
+The node server is in the `server/` directory and the react client is in the `client/` folder.
--- server/server.js
+++ server/server.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const routes = require('./routes/routes.js');
 const app = express();
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const morgan = require('morgan');
-const cors = require('cors');
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(
-  session({
-    secret: 'some secret key',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
-
 app.use(morgan('tiny'));
+
 
 /** ENDPOINTS **/
 app.get('/listtodos', routes.listTodos)
